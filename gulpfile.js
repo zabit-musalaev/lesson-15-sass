3e7fad03286abb11b4a1434d63e64f680285b2cd
zabit-musalaev <zabit.mv@gmail.com>
2020-03-05 23:29:24 +0300

    feat: sass

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const {src, dest, watch} = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// Static server
+function bs() {
+  serveSass();
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
